d3b09f4d5bf427dc3838b5438666f97f91c54e74
Miri Kim <miri.kim@onthebeach.co.uk>
2015-11-27 16:10:29 +0000

    Put each module and classe in a different file

---
+++ game-of-life/cell_pattern.rb
@@ -0,0 +1,30 @@
+module CellPattern
+  def self.blinker
+    c1 = Cell.new(CellState::ALIVE, [31,24])
+    c2 = Cell.new(CellState::ALIVE, [32,24])
+    c3 = Cell.new(CellState::ALIVE, [33,24])
+    [c1, c2, c3]
+  end
+
+  def self.toad
+    c1 = Cell.new(CellState::ALIVE, [31,25])
+    c2 = Cell.new(CellState::ALIVE, [32,25])
+    c3 = Cell.new(CellState::ALIVE, [32,24])
+    c4 = Cell.new(CellState::ALIVE, [33,25])
+    c5 = Cell.new(CellState::ALIVE, [33,24])
+    c6 = Cell.new(CellState::ALIVE, [34,24])
+    [c1, c2, c3, c4, c5, c6]
+  end
+
+  def self.beacon
+    c1 = Cell.new(CellState::ALIVE, [31,23])
+    c2 = Cell.new(CellState::ALIVE, [32,23])
+    c3 = Cell.new(CellState::ALIVE, [31,22])
+    c4 = Cell.new(CellState::ALIVE, [32,22])
+    c5 = Cell.new(CellState::ALIVE, [33,21])
+    c6 = Cell.new(CellState::ALIVE, [34,21])
+    c7 = Cell.new(CellState::ALIVE, [33,20])
+    c8 = Cell.new(CellState::ALIVE, [34,20])
+    [c1, c2, c3, c4, c5, c6, c7, c8]
+  end
+end
--- game-of-life/game_of_life_sim.rb
+++ game-of-life/game_of_life_sim.rb
@@ -1,44 +1,13 @@
 require 'gosu'
 require './game_of_life'
-
-module CellPattern
-  def self.blinker
-    c1 = Cell.new(CellState::ALIVE, [31,24])
-    c2 = Cell.new(CellState::ALIVE, [32,24])
-    c3 = Cell.new(CellState::ALIVE, [33,24])
-    [c1, c2, c3]
-  end
-
-  def self.toad
-    c1 = Cell.new(CellState::ALIVE, [31,25])
-    c2 = Cell.new(CellState::ALIVE, [32,25])
-    c3 = Cell.new(CellState::ALIVE, [32,24])
-    c4 = Cell.new(CellState::ALIVE, [33,25])
-    c5 = Cell.new(CellState::ALIVE, [33,24])
-    c6 = Cell.new(CellState::ALIVE, [34,24])
-    [c1, c2, c3, c4, c5, c6]
-  end
-
-  def self.beacon
-    c1 = Cell.new(CellState::ALIVE, [31,23])
-    c2 = Cell.new(CellState::ALIVE, [32,23])
-    c3 = Cell.new(CellState::ALIVE, [31,22])
-    c4 = Cell.new(CellState::ALIVE, [32,22])
-    c5 = Cell.new(CellState::ALIVE, [33,21])
-    c6 = Cell.new(CellState::ALIVE, [34,21])
-    c7 = Cell.new(CellState::ALIVE, [33,20])
-    c8 = Cell.new(CellState::ALIVE, [34,20])
-    [c1, c2, c3, c4, c5, c6, c7, c8]
-  end
-end
+require './cell'
+require './cell_pattern'
 
 class SimWindow < Gosu::Window
 
   @@w = 640
   @@h = 480
   
-  include CellPattern
-
   def initialize(pattern)
     super @@w, @@h
     self.caption = "Ruby :: Gosu :: Game of Life"
