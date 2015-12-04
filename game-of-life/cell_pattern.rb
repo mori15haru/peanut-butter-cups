928a07ee5dcb3f49d4035d209693d45cddc47904
Miri Kim <miri.kim@onthebeach.co.uk>
2015-12-04 14:43:46 +0000

    Add a new patter - glinder

--- game-of-life/cell_pattern.rb
+++ game-of-life/cell_pattern.rb
@@ -1,4 +1,14 @@
 module CellPattern
+  #Still lifes
+  def self.block
+    c1 = Cell.new(CellState::ALIVE, [31,24])
+    c2 = Cell.new(CellState::ALIVE, [32,24])
+    c3 = Cell.new(CellState::ALIVE, [31,25])
+    c4 = Cell.new(CellState::ALIVE, [32,25])
+    [c1, c2, c3, c4]
+  end
+
+  #Oscillator
   def self.blinker
     c1 = Cell.new(CellState::ALIVE, [31,24])
     c2 = Cell.new(CellState::ALIVE, [32,24])
@@ -26,5 +36,15 @@ module CellPattern
     c7 = Cell.new(CellState::ALIVE, [33,20])
     c8 = Cell.new(CellState::ALIVE, [34,20])
     [c1, c2, c3, c4, c5, c6, c7, c8]
+  end
+
+  #Spaceships
+  def self.glinder
+    c1 = Cell.new(CellState::ALIVE, [5,5])
+    c2 = Cell.new(CellState::ALIVE, [3,6])
+    c3 = Cell.new(CellState::ALIVE, [5,6])
+    c4 = Cell.new(CellState::ALIVE, [4,7])
+    c5 = Cell.new(CellState::ALIVE, [5,7])
+    [c1, c2, c3, c4, c5]
   end
 end
